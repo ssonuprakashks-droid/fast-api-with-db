c088b81962325e4ebdc14e74aad7b873228e833f
sonu prakash k.s <ssonuprakashks@gmail.com>
2026-02-09 09:07:42 +0530

    ten

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -7,3 +7,5 @@ class userRepo:
         self.db.add(user)
         self.db.commit()
         return user
+    def get_user_by_email(self,email:str):
+        return self.db.query(User).filter(User.email==email).first()
--- schemas/user_schemas.py
+++ schemas/user_schemas.py
@@ -1,4 +1,4 @@
 from pydantic import BaseModel
-class user_schemas(BaseModel):
+class UserSchema(BaseModel):
     email:str
     password:str
